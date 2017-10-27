0bf44c4a80f419597e532e6d122ad44b9c42972f
Simon Adcock <simon.adcock@guardian.co.uk>
2017-10-27 15:51:28 +0100

    convert inputstream to string

--- src/main/scala/com.gu/simonAws/Lambda.scala
+++ src/main/scala/com.gu/simonAws/Lambda.scala
@@ -3,11 +3,14 @@ package com.gu.simonAws
 import io.circe.syntax._
 import java.io._
 import java.nio.charset.StandardCharsets.UTF_8
+
 import com.typesafe.scalalogging.LazyLogging
+
+import scala.io.Source
 
 object Lambda extends LazyLogging {
   def handler(in: InputStream, out: OutputStream): Unit = {
-    val received = in.toString()
+    val received = Source.fromInputStream(in).mkString
     val response = APIResponse(200,  Map("Content-Type" -> "application/json"), received)
     //no spaces converts json to a string
     out.write(response.asJson.noSpaces.getBytes(UTF_8))
